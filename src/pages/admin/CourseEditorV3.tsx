abf647f112c41ffd024a3b2bdda2963277be019e
gafniasaf <agafni@expertcollege.com>
2025-12-26 12:41:15 +0100

    Make option action icons always visible (not just on hover)

--- src/pages/admin/CourseEditorV3.tsx
+++ src/pages/admin/CourseEditorV3.tsx
@@ -2206,7 +2206,7 @@ const CourseEditorV3 = () => {
                                             data-action="edit"
                                           />
 
-                                          <div className="flex items-center gap-1 opacity-0 group-hover:opacity-100 transition-opacity">
+                                          <div className="flex items-center gap-1">
                                             <button
                                               type="button"
                                               onClick={() => handleAIRewriteOption(idx)}
